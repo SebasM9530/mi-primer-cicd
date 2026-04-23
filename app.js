c0995fd2505545bf089ed5711160fc196c506bb6
SebasM9530 <162827826+SebasM9530@users.noreply.github.com>
2026-04-23 21:46:07 +0000

    feat: agrego servidor web para despliegue

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const http = require('http');
+
 function sumar(a, b) {
   return a + b;
 }
@@ -5,5 +7,15 @@ function sumar(a, b) {
 function holaMundo() {
   return "Hola DevOps";
 }
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(holaMundo());
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
 
 module.exports = { sumar, holaMundo };
